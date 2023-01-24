fab6bc65adee15df22a142065b666fe3a9a9686a
horaciosg96 <horaciosg96@gmail.com>
2023-01-24 14:34:00 -0600

    commit3

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log();
+const formulario = document.getElementById('formulario')
+const input = document.getElementById('input')
+const listaTarea = document.getElementById('lista-tareas')
+const template = document.getElementById('template').content
+const fragment = document.createDocumentFragment()
+let tareas = {}
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault()
+
+    setTarea()
+})
+
+const setTarea = e => {
+    if (input.value.trim() === '') {
+        return
+    }
+
+    const tarea = {
+        id: Date.now(),
+        texto: input.value,
+        estado: false
+    }
+
+    tareas[tarea.id] = tarea
+    formulario.reset()
+    input.focus()
+
+    const pintarTareas = () => {
+        listaTarea.innerHTML = ''
+        Object.values(tareas).forEach(tarea => {
+            const clone = template.cloneNode(true)
+            clone.querySelector('p').textContent = tarea.texto
+            fragment.appendChild(clone)
+        })
+    }
+    listaTarea.appendChild(fragment)
+}
